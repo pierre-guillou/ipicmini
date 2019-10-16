9357f1731f67ab263daa3b48806239d4ccb74a09
Pierre Guillou <pierre.guillou@lip6.fr>
2019-10-16 11:30:46 +0200

    [Adaptor] Guard against empty finalize script

--- src/catalyst/Adaptor.cxx
+++ src/catalyst/Adaptor.cxx
@@ -158,11 +158,18 @@ void Initialize(const char *script, const int start_x, const int start_y,
 //----------------------------------------------------------------------------
 void Finalize(const std::string &script)
 {
-  vtkNew<vtkCPPythonScriptPipeline> pipeline;
-  pipeline->Initialize(script.c_str());
-  Processor->AddPipeline(pipeline);
-  vtkNew<vtkCPDataDescription> dataDescription;
-  Processor->CoProcess(dataDescription);
+  if (!script.empty()) {
+    vtkNew<vtkCPPythonScriptPipeline> pipeline;
+    pipeline->Initialize(script.c_str());
+    Processor->AddPipeline(pipeline);
+    vtkNew<vtkCPDataDescription> dataDescription;
+    dataDescription->AddInput(InputName);
+    dataDescription->SetTimeData(0.0, 0);
+    auto idd = dataDescription->GetInputDescriptionByName(InputName);
+    idd->SetGrid(VTKGrid);
+
+    Processor->CoProcess(dataDescription);
+  }
 
   if (Processor)
   {
