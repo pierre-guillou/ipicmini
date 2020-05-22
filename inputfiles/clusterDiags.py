7a0d5346ffc2436ee38b00afe0df68e50c126824
Pierre Guillou <pierre.guillou@lip6.fr>
2020-05-23 00:48:19 +0200

    [clusterDiags] Fix/silence pylint warnings, improve SQL concatenation

--- inputfiles/clusterDiags.py
+++ inputfiles/clusterDiags.py
@@ -1,22 +1,23 @@
-from paraview.simple import *
+# pylint: disable=invalid-name, missing-docstring, import-error
+from paraview import simple
 
 # read every other persistence diagram
-CinemaReader = TTKCinemaReader(DatabasePath="data/pdiags.cdb")
-CinemaFilter = TTKCinemaQuery(InputTable=CinemaReader)
-CinemaFilter.SQLStatement = ("SELECT it.*, printf("
-                             "  %s_%s_%s_%s_%s, "
-                             "  it.CaseName, it.B0x, it.B0y, it.B0z, it.ns"
-                             ") AS SimParams "
-                             "FROM InputTable0 AS it "
-                             "WHERE TimeStep % 2 = 0")
-ProductReader = TTKCinemaProductReader(Input=CinemaFilter)
+CinemaReader = simple.TTKCinemaReader(DatabasePath="data/pdiags.cdb")
+CinemaFilter = simple.TTKCinemaQuery(InputTable=CinemaReader)
+CinemaFilter.SQLStatement = """SELECT it.*, printf(
+  "%s_%s_(%s-%s-%s)_%s",
+  it.CaseName, it.ScalarField, it.B0x, it.B0y, it.B0z, it.ns
+) AS Case_Field_B0_ns
+FROM InputTable0 AS it
+WHERE TimeStep % 2 = 0"""
+ProductReader = simple.TTKCinemaProductReader(Input=CinemaFilter)
 
 # get a distance matrix from these diagrams
-DistMat = TTKPersistenceDiagramDistanceMatrix(Input=ProductReader)
+DistMat = simple.TTKPersistenceDiagramDistanceMatrix(Input=ProductReader)
 DistMat.NumberofPairs = 20
 
 # reduce the distance matrix to 3D coordinates
-DimRed = TTKDimensionReduction(Input=DistMat)
+DimRed = simple.TTKDimensionReduction(Input=DistMat)
 DimRed.SelectFieldswithaRegexp = 1
 DimRed.Regexp = "Diagram.*"
 DimRed.Components = 3
@@ -24,14 +25,14 @@ DimRed.InputIsaDistanceMatrix = 1
 DimRed.UseAllCores = 0
 
 # generate points from 3D coordinates
-t2p = TableToPoints(Input=DimRed)
+t2p = simple.TableToPoints(Input=DimRed)
 t2p.XColumn = "Component_0"
 t2p.YColumn = "Component_1"
 t2p.ZColumn = "Component_2"
 t2p.KeepAllDataArrays = 1
-tetra = Tetrahedralize(Input=t2p)
+tetra = simple.Tetrahedralize(Input=t2p)
 
 # generate a heat map from distance matrix
 # save output distance matrix, heat map and point cloud
-SaveData("data/distmat.csv", Input=DimRed)
-SaveData("data/distmat.vtu", CleantoGrid(Input=tetra))
+simple.SaveData("data/distmat.csv", Input=DimRed)
+simple.SaveData("data/distmat.vtu", simple.CleantoGrid(Input=tetra))
